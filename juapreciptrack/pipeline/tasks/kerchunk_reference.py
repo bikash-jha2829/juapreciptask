87860ae7ddfa5203e00ae72aa4d2883d4ace562b
bikash-jha2829 <79247013+bikash-jha2829@users.noreply.github.com>
2024-09-02 01:57:27 +0200

    Add files via upload

--- juapreciptrack/pipeline/tasks/kerchunk_reference.py
+++ juapreciptrack/pipeline/tasks/kerchunk_reference.py
@@ -4,7 +4,15 @@ from kerchunk.netCDF3 import NetCDF3ToZarr
 
 @ray.remote
 def generate_and_persist_kerchunk_reference(file_url):
-    """Generate Kerchunk reference for a NetCDF file and persist it using Ray."""
+    """
+    Generate a Kerchunk reference for a NetCDF file and persist it using Ray.
+
+    Args:
+        file_url (str): The URL of the NetCDF file in cloud storage.
+
+    Returns:
+        dict: A dictionary representing the Kerchunk reference.
+    """
     full_url = f"gs://{file_url}"
     reference = NetCDF3ToZarr(full_url, storage_options={"anon": True}).translate()
     return reference
--- juapreciptrack/pipeline/tasks/transform_task.py
+++ juapreciptrack/pipeline/tasks/transform_task.py
@@ -5,7 +5,17 @@ from config.config import CHUNK_SIZE
 
 
 def extract_relevant_data(ds, h3_resolution=10, output_path="output.parquet"):
-    """Compute H3 index directly within an xarray.Dataset and extract relevant data, then save to Parquet."""
+    """
+    Compute H3 index directly within an xarray.Dataset, extract relevant data, and save the results to Parquet.
+
+    Args:
+        ds (xr.Dataset): The xarray Dataset containing the data to be processed.
+        h3_resolution (int): The resolution of the H3 index. Default is 10.
+        output_path (str): The path where the Parquet file should be saved. Default is "output.parquet".
+
+    Returns:
+        dd.DataFrame: A Dask DataFrame containing the extracted and processed data.
+    """
 
     # Function to compute H3 index
     def compute_h3(lat, lon):
